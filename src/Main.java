e83c5320ee7a6a32c1136870ed3923b13aba5029
Exwithe <remo.oberschwand@gmail.com>
2019-06-26 22:55:58 +0200

    Updatet System: hight = Max Anzahl Sterne in Reihe star=Anzahl Sterne

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,28 @@ public class Main {
     //file formatting
     public static void main(String[] args) {
 
-            int l = 1;
-            int dir = 1; 
-            for(int enter = 0; enter < 6; enter++) { // die beiden fors von line 8, 19 sehen doch gleich aus nicht kann man das nicht noch abändern
+        int hight = 19;
 
-
-                for(int space = 0; space < l; space++) {
-                    System.out.print("***");
+        int l = 1;
+        for (int enter = 0; enter < hight * 2; enter++) {
+            for (int space = 0; space < l; space++) {
+                for (int star = 1; star != 0; star--) {
+                    System.out.print("*");
                 }
-                l = l +  dir; //man l = l + dir; anders schreiben kann
-                System.out.println();
-
             }
-            dir = -1; // warum nicht dir 1 lassen und einfach
-            for(int i = 0; i < 7; i++) {
-
-
-                for(int j = 0; j < l; j++) {
-                    System.out.print("***");
-
-                }
-                l = l +  dir; // wechseln auf sehe 17 line zudem überlege dir wie man l = l + dir; anders schreiben kann
-                System.out.println();
+            System.out.println();
+            if (enter + 1 >= hight)
+            {
+                l--;
+            }
+                else {
+                l++;
             }
         }
 
+
     }
 
+
+}
 
