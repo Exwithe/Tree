98833853cf28e98851d1057b305c959e364d9d86
sirh3e <marvin.henk.pro@gmail.com>
2019-06-26 21:44:28 +0200

    Added notes

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
 
+    //file formatting
     public static void main(String[] args) {
 
             int l = 1;
-            int dir = +1;
-            for(int i = 0; i < 6; i++) {
+            int dir = +1; //warum das +?
+            for(int i = 0; i < 6; i++) { // die beiden fors von line 8, 19 sehen doch gleich aus nicht kann man das nicht noch abändern
 
 
                 for(int j = 0; j < l; j++) {
                     System.out.print("***");
 
                 }
-                l = l +  dir;
+                l = l +  dir; //man l = l + dir; anders schreiben kann
                 System.out.println();
             }
-            dir = -1;
+            dir = -1; // warum nicht dir 1 lassen und einfach
             for(int i = 0; i < 7; i++) {
 
 
@@ -22,7 +23,7 @@ public class Main {
                     System.out.print("***");
 
                 }
-                l = l +  dir;
+                l = l +  dir; // wechseln auf sehe 17 line zudem überlege dir wie man l = l + dir; anders schreiben kann
                 System.out.println();
             }
         }
