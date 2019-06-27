dfe10b883efc0906d7085758e677ddf9fb725e24
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-06-27 10:39:42 +0200

    move file and replace tabs with spaces

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Main {
+  public static void main(String[] args) {
+    int height = 20;
+    int lineLength = 1;
+    for (int lineCount = 0; lineCount < height * 2; lineCount++) {
+      for (int charCount = 0; charCount < lineLength; charCount++) {
+        System.out.print("*");
+      }
+      System.out.println();
+      if (lineCount + 1 < height) {
+        lineLength++;
+      } else {
+        lineLength--;
+      }
+    }
+  }
+}
