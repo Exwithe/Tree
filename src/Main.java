56f31bc2e5d1de9c93c8db1ccd9a3a6ddf61670d
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-06-27 10:30:46 +0200

    refactor variable names and delete usless loop

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
 public class Main {
 
-    public static void main(String[] args) {
-
-        int hight = 19;
-        int l = 1;
-        for (int enter = 0; enter < hight * 2; enter++) {
-            for (int space = 0; space < l; space++) {
-                for (int star = 1; star != 0; star--) {
-                    System.out.print("*");
-                }
-            }
-            System.out.println();
-            if (enter + 1 >= hight) { l--;
-            }
-            else { l++;
-            }
-        }
-    }
+	public static void main(String[] args) {
+		int height = 20;
+		int lineLength = 1;
+		for (int lineCount = 0; lineCount < height * 2; lineCount++) {
+			for (int charCount = 0; charCount < lineLength; charCount++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			if (lineCount + 1 < height) {
+				lineLength++;
+			} else {
+				lineLength--;
+			}
+		}
+	}
 }
-
